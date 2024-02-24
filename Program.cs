a2eab01386be45577a41156b7a0dab0d8d7d7de8
Peniel kaluwa <kaluwapeniel@gmail.com>
2024-02-24 17:18:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using DZen.Security.Cryptography;
+//using System.Security.Cryptography;
+//using System.Security.Cryptography.HashAlgorithm;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"D:\BADIBANGA_KALUWA_PENIEL\ITRANSITION\Intership_Project\Task-2\task2";
+            string email = "badibangakaluwapeniel@gmail.com";
+            var files = Directory.GetFiles(folderPath).OrderBy(f => f);
+
+            string[] hashes = new string[files.Count()];
+
+            for (int i = 0; i < files.Count(); i++)
+            {
+                using (var stream = File.OpenRead(files.ElementAt(i)))
+                {
+                    byte[] fileBytes = new byte[stream.Length];
+                    stream.Read(fileBytes, 0, fileBytes.Length);
+                    using (var sha3 = SHA3.Create())
+                    {
+                        byte[] fileHashBytes = sha3.ComputeHash(fileBytes);
+                        hashes[i] = BitConverter.ToString(fileHashBytes).Replace("-", "").ToLower();
+                    }
+                }
+            }
+
+            Array.Sort(hashes);
+
+            string resultHash = string.Join("", hashes) + email.ToLower();
+
+            using (var sha3 = SHA3.Create())
+            {
+                byte[] resultHashBytes = sha3.ComputeHash(System.Text.Encoding.ASCII.GetBytes(resultHash));
+                string finalHash = BitConverter.ToString(resultHashBytes).Replace("-", "").ToLower();
+
+                Console.WriteLine(finalHash);
+            }
+        }
+    }
+}
+
